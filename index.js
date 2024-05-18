1fce852aab1bc3e328448fa058157fea8c3ac713
OcéaneJF <oceanejf974@gmail.com>
2024-05-19 00:50:17 +0200

    add socket but do not work

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ import {body, validationResult} from "express-validator";
 import bodyParser from "body-parser";
 import express from "express";
 import bcrypt from "bcrypt";
+import http from "http";
+import {Server} from "socket.io";
 
 var app = express();
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors: {
+        allowedHeaders: '*',
+        origin:'*'
+    }
+});
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -12,28 +21,19 @@ const con = mysql.createConnection({
     database: "todomulti"
 });
 
+io.on('d', (socket) => {
+    console.log('a user connected');
+});
 
 app.listen(8000, () => {
     console.log("Server running on port 8000");
 });
-
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 app.use(bodyParser.json());
-
-app.get("/users",async (req, res, next) => {
-    console.log(hashPassword("qsdsqqs"))
-    con.connect(function(err) {
-        if (err) throw err;
-        con.query("SELECT * FROM users", function (err, result, fields) {
-            if (err) throw err;
-            res.json(result);
-        });
-    });
-});
 
 app.post("/users",[
     body('username').trim().isAlphanumeric().escape(),
